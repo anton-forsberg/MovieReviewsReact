cc3543f42a9070b83d4e4b764b473ae583bef6ba
midghaar <anton_molgan@hotmail.com>
2019-04-24 00:04:35 +0200

    Updates

---
+++ precache-manifest.ede622f7f465aec0ff68dfa8427903c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867422330e1933ad0f5dd742ad286986",
+    "url": "/MovieReviewsReact/index.html"
+  },
+  {
+    "revision": "de420a64f6fe9aad148c",
+    "url": "/MovieReviewsReact/static/css/main.82d176dc.chunk.css"
+  },
+  {
+    "revision": "772e8e6f33ce5be03245",
+    "url": "/MovieReviewsReact/static/js/2.65b9c135.chunk.js"
+  },
+  {
+    "revision": "de420a64f6fe9aad148c",
+    "url": "/MovieReviewsReact/static/js/main.41a51b60.chunk.js"
+  },
+  {
+    "revision": "07ef2da474e1b8fbb457",
+    "url": "/MovieReviewsReact/static/js/runtime~main.390bd4b1.js"
+  }
+]);
